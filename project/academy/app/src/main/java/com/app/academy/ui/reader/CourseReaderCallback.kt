7e9dc0755ca1107be34c1e8625078e97d0f21bae
blitzkz23 <blitzaldykazu@gmail.com>
2021-10-30 18:08:28 +0700

    Add academy project

---
+++ project/academy/app/src/main/java/com/app/academy/ui/reader/CourseReaderCallback.kt
@@ -0,0 +1,5 @@
+package com.app.academy.ui.reader
+
+interface CourseReaderCallback {
+	fun moveTo(position: Int, moduleId: String)
+}
---
+++ project/academy/app/src/main/java/com/app/academy/ui/reader/content/ModuleContentFragment.kt
@@ -0,0 +1,44 @@
+package com.app.academy.ui.reader.content
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.app.academy.R
+import com.app.academy.data.ContentEntity
+import com.app.academy.databinding.FragmentModuleContentBinding
+
+
+class ModuleContentFragment : Fragment() {
+
+	companion object {
+		val TAG: String = ModuleContentFragment::class.java.simpleName
+		fun newInstance(): ModuleContentFragment = ModuleContentFragment()
+	}
+
+	private lateinit var fragmentModuleContentBinding: FragmentModuleContentBinding
+
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		fragmentModuleContentBinding =
+			FragmentModuleContentBinding.inflate(inflater, container, false)
+		return fragmentModuleContentBinding.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		if (activity != null) {
+			val content =
+				ContentEntity("<h3 class=\\\"fr-text-bordered\\\">Contoh Content</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>")
+			populateWebView(content)
+		}
+	}
+
+	private fun populateWebView(content: ContentEntity) {
+		fragmentModuleContentBinding.webView.loadData(content.content ?: "", "text/html", "UTF-8")
+	}
+
+}
