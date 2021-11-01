c0216ca55d1a784f45a174a2094f4e1c30eb2a29
blitzkz23 <blitzaldykazu@gmail.com>
2021-11-01 21:12:01 +0700

    Add splash screen and fix some of UI

--- submission1/app/src/main/java/com/app/themoviedatabase/ui/home/HomeActivity.kt
+++ submission1/app/src/main/java/com/app/themoviedatabase/ui/home/HomeActivity.kt
@@ -2,11 +2,13 @@ package com.app.themoviedatabase.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.app.themoviedatabase.R
 import com.app.themoviedatabase.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+		setTheme(R.style.Theme_TheMovieDatabase)
 
 		val activityHomeBinding = ActivityHomeBinding.inflate(layoutInflater)
 		setContentView(activityHomeBinding.root)
