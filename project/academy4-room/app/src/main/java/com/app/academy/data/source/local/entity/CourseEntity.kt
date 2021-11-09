62f479b15810f6cad936e0da6f650f487bd88c24
blitzkz23 <blitzaldykazu@gmail.com>
2021-11-09 18:12:37 +0700

    Init academy project using room

---
+++ project/academy4-room/app/src/main/java/com/app/academy/data/source/local/entity/CourseEntity.kt
@@ -0,0 +1,10 @@
+package com.app.academy.data.source.local.entity
+
+data class CourseEntity(
+	var courseId: String,
+	var title: String,
+	var description: String,
+	var deadline: String,
+	var bookmarked: Boolean = false,
+	var imagePath: String
+)
