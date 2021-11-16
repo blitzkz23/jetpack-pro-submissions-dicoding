c44d5970cda451582fb0d019f506c89502a4f8a3
blitzkz23 <blitzaldykazu@gmail.com>
2021-11-16 14:49:38 +0700

    Replace callback in remote data source with api response

--- submission3/app/src/main/java/com/app/themoviedatabase/data/source/remote/RemoteDataSource.kt
+++ submission3/app/src/main/java/com/app/themoviedatabase/data/source/remote/RemoteDataSource.kt
@@ -2,6 +2,8 @@ package com.app.themoviedatabase.data.source.remote
 
 import android.content.ContentValues.TAG
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.app.themoviedatabase.BuildConfig
 import com.app.themoviedatabase.api.ApiConfig
 import com.app.themoviedatabase.data.source.remote.response.ResultsItem
@@ -13,49 +15,48 @@ import retrofit2.awaitResponse
 
 class RemoteDataSource {
 
-	fun getPopularMovie(callback: PopularCallback) {
+	fun getPopularMovie() : LiveData<ApiResponse<List<ResultsItem>>> {
 		EspressoIdlingResource.increment()
+		val resultMovie = MutableLiveData<ApiResponse<List<ResultsItem>>>()
 		CoroutineScope(Dispatchers.IO).launch {
 			val client = ApiConfig.getApiService().getPopular("movie", BuildConfig.MOVIEDB_TOKEN, 1)
 			try {
 				val response = client.awaitResponse()
 				if (response.isSuccessful) {
 					response.body()?.results?.let {
-						callback.onPopularDataReceived(it)
+						resultMovie.value = ApiResponse.success(it)
 						EspressoIdlingResource.decrement()
 					}
 				} else {
 					Log.d(TAG, "onResponse: ${response.message()}")
 				}
 			} catch (e: Exception) {
-				callback.onDataNotAvailable()
+				Log.d(TAG, "Exception: ${e.message}")
 			}
 		}
+		return resultMovie
 	}
 
-	fun getPopularTvShow(callback: PopularCallback) {
+	fun getPopularTvShow() : LiveData<ApiResponse<List<ResultsItem>>> {
 		EspressoIdlingResource.increment()
+		val resultTvShow = MutableLiveData<ApiResponse<List<ResultsItem>>>()
 		CoroutineScope(Dispatchers.IO).launch {
 			val client = ApiConfig.getApiService().getPopular("tv", BuildConfig.MOVIEDB_TOKEN, 1)
 			try {
 				val response = client.awaitResponse()
 				if (response.isSuccessful) {
 					response.body()?.results?.let {
-						callback.onPopularDataReceived(it)
+						resultTvShow.value = ApiResponse.success(it)
 						EspressoIdlingResource.decrement()
 					}
 				} else {
 					Log.d(TAG, "onResponse: ${response.message()}")
 				}
 			} catch (e: Exception) {
-				callback.onDataNotAvailable()
+				Log.d(TAG, "Exception: ${e.message}")
 			}
 		}
-	}
-
-	interface PopularCallback {
-		fun onPopularDataReceived(popularResponses: List<ResultsItem>)
-		fun onDataNotAvailable()
+		return resultTvShow
 	}
 
 	companion object {
